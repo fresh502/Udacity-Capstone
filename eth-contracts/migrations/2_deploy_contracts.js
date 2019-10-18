2cf3b5af7ece28c459a225f5bfbc7a015c264d8c
Sinrok Kim <fresh502@gmail.com>
2019-10-18 11:47:22 +0900

    set truffle config to deploy on rinkeby

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ const SquareVerifier = artifacts.require("SquareVerifier");
 const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
 
 module.exports = (deployer) => {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier, SquareVerifier.address,'Udacity', 'UDC', 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/'); 
+  deployer.deploy(SquareVerifier).then(() => {
+    return deployer.deploy(SolnSquareVerifier, SquareVerifier.address,'Udacity', 'UDC', 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/'); 
+  })
 };
